f11b520970795dcd7e957146e9a0be707e7cf48c
SerhiiStepanovich <bysprint82@gmail.com>
2025-10-19 23:33:29 +0300

    correct-notepreview

--- app/@modal/(.)notes/NoteModal.client.tsx
+++ app/@modal/(.)notes/NoteModal.client.tsx
@@ -1,22 +1,7 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import Modal from "@/components/Modal/Modal";
 import NotePreview from "./[id]/NotePreview.client";
-import styles from "@/components/NotePreview/NotePreview.module.css";
 
 export default function NoteModalClient({ id }: { id: string }) {
-  const router = useRouter();
-  const handleClose = () => router.back();
-
-  return (
-    <Modal onClose={handleClose}>
-      <div className={styles.preview}>
-        <button onClick={handleClose} className={styles.closeButton}>
-          ✕ Закрити
-        </button>
-        <NotePreview id={id} />
-      </div>
-    </Modal>
-  );
+  return <NotePreview id={id} />;
 }
